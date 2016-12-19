feb296ccc3c5a53f9bde6d0b87177c0333042ea2
Marcin Giec <giecmarcin@gmail.com>
2016-12-19 17:08:02 +0100

    Remove problem with builtInMemory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,71 +13,75 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 public class Main {
 
     public static void main(String[] args) throws IOException, Exception {
+        //MotoDownloader motoDownloader = new MotoDownloader();
+        //motoDownloader.download("");
         List<Phone> allPhones = new ArrayList<Phone>();
-//        String url1 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?per_page=90";
-//        String url2 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=2&per_page=90";
-//        String url3 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=3&per_page=90";
-//        String url4 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=4&per_page=90";
-//        String url5 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=5&per_page=90";
-
-
-        String url1 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?per_page=90&f[201][61322]=1&f[201][61325]=1&f[201][61324]=1&f[201][61323]=1";
-        String url2 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=2&per_page=90&f[201][61322]=1&f[201][61325]=1&f[201][61324]=1&f[201][61323]=1";
-        String url3 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=3&per_page=90&f[201][61322]=1&f[201][61325]=1&f[201][61324]=1&f[201][61323]=1";
-        String url4 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=4&per_page=90&f[201][61322]=1&f[201][61325]=1&f[201][61324]=1&f[201][61323]=1";
-        String url5 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=5&per_page=90&f[201][61322]=1&f[201][61325]=1&f[201][61324]=1&f[201][61323]=1";
-
-        System.out.println("Hello World!");
-        PhonesDownloader phonesDownloader = new PhonesDownloader();
-        allPhones.addAll(phonesDownloader.download(url1));
-        allPhones.addAll(phonesDownloader.download(url2));
-        allPhones.addAll(phonesDownloader.download(url3));
-        allPhones.addAll(phonesDownloader.download(url4));
-        allPhones.addAll(phonesDownloader.download(url5));
-
-        int id = 1;
-        for (Phone p : allPhones) {
-            p.setId(id);
-            id += 1;
-
-            if(p.getColour().isEmpty()){
-                p.setColour(randomColour());
-            }
-            if (checkTextHaveNumbers(p.getColour())) {
-                p.setColour(randomColour());
-            }
-
-            if (p.getColour().toLowerCase().contains("czujnik")) {
-                p.setColour(randomColour());
-            }
-
-            String temp = p.getFullName().replaceAll("\\s+", "");
-            String jpg1 = temp + "Img1";
-            String jpg2 = temp + "Img2";
-            String jpg3 = temp + "Img3";
-            p.setImagesUrl(new String[]{jpg1, jpg2, jpg3});
-        }
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        //Set pretty printing of json
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        String arrayToJson = "";
-        try {
-            FileWriter fw = new FileWriter("phones-data.json");
-            arrayToJson = objectMapper.writeValueAsString(allPhones);
-            System.out.println("1. Convert List of person objects to JSON :");
-            System.out.println(arrayToJson);
-            fw.write(arrayToJson);
-            fw.close();
-            System.out.println("Done " + allPhones.size());
-            System.out.println("Błędy: " + phonesDownloader.numberOfErrors);
-        } catch (JsonGenerationException e) {
-            e.printStackTrace();
-        } catch (JsonMappingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+////        String url1 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?per_page=90";
+////        String url2 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=2&per_page=90";
+////        String url3 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=3&per_page=90";
+////        String url4 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=4&per_page=90";
+////        String url5 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=5&per_page=90";
+//
+//
+       String url1 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?per_page=90&f[201][61322]=1&f[201][61325]=1&f[201][61324]=1&f[201][61323]=1";
+//        String url2 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=2&per_page=90&f[201][61322]=1&f[201][61325]=1&f[201][61324]=1&f[201][61323]=1";
+//        String url3 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=3&per_page=90&f[201][61322]=1&f[201][61325]=1&f[201][61324]=1&f[201][61323]=1";
+//        String url4 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=4&per_page=90&f[201][61322]=1&f[201][61325]=1&f[201][61324]=1&f[201][61323]=1";
+//        String url5 = "http://www.x-kom.pl/g-4/c/1590-smartfony-i-telefony.html?page=5&per_page=90&f[201][61322]=1&f[201][61325]=1&f[201][61324]=1&f[201][61323]=1";
+//
+//        System.out.println("Hello World!");
+       PhonesDownloader phonesDownloader = new PhonesDownloader();
+       phonesDownloader.download(url1);
+       phonesDownloader.convertToList();
+      //allPhones.addAll(phonesDownloader.download(url1));
+//        allPhones.addAll(phonesDownloader.download(url2));
+//        allPhones.addAll(phonesDownloader.download(url3));
+//        allPhones.addAll(phonesDownloader.download(url4));
+//        allPhones.addAll(phonesDownloader.download(url5));
+//
+//        int id = 1;
+//        for (Phone p : allPhones) {
+//            p.setId(id);
+//            id += 1;
+//
+//            if(p.getColour().isEmpty()){
+//                p.setColour(randomColour());
+//            }
+//            if (checkTextHaveNumbers(p.getColour())) {
+//                p.setColour(randomColour());
+//            }
+//
+//            if (p.getColour().toLowerCase().contains("czujnik")) {
+//                p.setColour(randomColour());
+//            }
+//
+//            String temp = p.getFullName().replaceAll("\\s+", "");
+//            String jpg1 = temp + "Img1";
+//            String jpg2 = temp + "Img2";
+//            String jpg3 = temp + "Img3";
+//            p.setImagesUrl(new String[]{jpg1, jpg2, jpg3});
+//        }
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        //Set pretty printing of json
+//        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+//        String arrayToJson = "";
+//        try {
+//            FileWriter fw = new FileWriter("phones-data.json");
+//            arrayToJson = objectMapper.writeValueAsString(allPhones);
+//            System.out.println("1. Convert List of person objects to JSON :");
+//            System.out.println(arrayToJson);
+//            fw.write(arrayToJson);
+//            fw.close();
+//            System.out.println("Done " + allPhones.size());
+//            System.out.println("Błędy: " + phonesDownloader.numberOfErrors);
+//        } catch (JsonGenerationException e) {
+//            e.printStackTrace();
+//        } catch (JsonMappingException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     private static boolean checkTextHaveNumbers(String text) {
--- src/main/java/PhonesDownloader.java
+++ src/main/java/PhonesDownloader.java
@@ -4,211 +4,91 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhonesDownloader {
+    TreeMap<String, String> values = new TreeMap<>();
 
-    boolean addToList = true;
-    public int numberOfErrors = 0;
-
-    public List<Phone> download(String url) throws IOException {
-
+    public TreeMap<String, String> download(String url) throws IOException {
+//        TreeMap<String, String> values = new TreeMap<>();
         Connection connect = Jsoup.connect(url).timeout(10 * 1000);
         Document document = connect.get();
         Elements all = document.getElementsByClass("product-item");//.tagName("data-product-name");//data-products-list-name=
 
-        //System.out.println(all.toString());
-        List<Phone> phones = new ArrayList<Phone>();
-        //System.out.println("Ilosć:  " + all.size());
+        for (int i = 0; i < 1; i++) {
+            System.out.println("Numer: " + i);
+            String brand = all.get(i).attr("data-product-brand");
+            values.put("Producent", brand);
+
+            String fullName = all.get(i).attr("data-product-name");
+            values.put("Nazwa", fullName);
+            String price = all.get(i).attr("data-product-price");
+            values.put("Cena", price);
+            String urlDetails = all.get(i).getElementsByClass("description-wrapper").select("a").first().attr("abs:href");
+            values.put("xKomURL", urlDetails);
+
+            //For read details
+            Connection connect2 = Jsoup.connect(urlDetails).timeout(10 * 1000);
+            Document document2 = connect2.get();
+            Elements allDetailsForPhone = document2.getElementsByClass("js-table-preview");
+
+            int numberOfRows = allDetailsForPhone.select("table").get(i).select("th").size();
+            List<String> keys = new ArrayList<>();
+            for (int m = 0; m < numberOfRows; m++) {
+                String key = allDetailsForPhone.select("table").get(i).select("th").get(m).text();
+                String val = allDetailsForPhone.select("table").get(i).getElementsByClass("col-xs-9").get(m).text();
+                values.put(key, val);
+            }
+
+
+        }
+//        for (Map.Entry<String, String> entry : values.entrySet()) {
+//            System.out.println(entry.getKey() + ": " + entry.getValue());
+//        }
+        return values;
+    }
+
+    public void convertToList() {
+        List<Phone> phones = new ArrayList<>();
         Phone phone = null;
+        for (Map.Entry<String, String> entry : values.entrySet()) {
+            phone = new Phone();
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (key.contains("Aparat")) {
+                System.out.println("Tutaj");
+                String[] temp = value.split("\\s+");
+                List<Double> camera = new ArrayList<>();
+                for (String s : temp) {
+                    try{
+                        double tempCamera = Double.parseDouble(s.replaceAll(",","."));
+                        camera.add(tempCamera);
+                    }catch (Exception e){
 
-        for (int i = 0; i < all.size(); i++) {
-            try {
-                System.out.println(i);
-                phone = new Phone();
-                String brand = all.get(i).attr("data-product-brand");//ok
-                phone.setBrand(brand);
-                String fullName = all.get(i).attr("data-product-name");//ok
-                phone.setFullName(fullName);
-                double price = Double.parseDouble(all.get(i).attr("data-product-price")); //ok
-                phone.setPrice(price);
-                String urlDetails = all.get(i).getElementsByClass("description-wrapper").select("a").first().attr("abs:href");
-
-                String tempDisplayOfSize = all.get(i).getElementsByClass("features").select("ul").select("li").get(0).text();
-                tempDisplayOfSize = tempDisplayOfSize.replaceAll(",", ".");
-                //System.out.println(findNumberInString(tempDisplayOfSize));
-                phone.setSizeOfDisplay(findNumberInString(tempDisplayOfSize));
-
-                String processor = all.get(i).getElementsByClass("features").select("ul").select("li").get(1).text();
-                phone.setProcessor(processor);
-                String tempBuiltInMemory = all.get(i).getElementsByClass("features").select("ul").select("li").get(2).text();
-                tempBuiltInMemory = tempBuiltInMemory.replaceAll(",", ".");
-                if (tempBuiltInMemory.contains("+")) {
-                    int index = tempBuiltInMemory.indexOf("+");
-                    if (index != -1) {
-                        tempBuiltInMemory = tempBuiltInMemory.substring(0, index);
                     }
                 }
-                phone.setBuiltInMemory(findNumberInString(tempBuiltInMemory));
-                String operatingSystem = all.get(i).getElementsByClass("features").select("ul").select("li").get(3).text();
-                phone.setOperatingSystem(operatingSystem);
-                //System.out.println(all.get(i).getElementsByClass("features").select("ul").select("li").get(3).text());
-
-
-                //For read details
-                Connection connect2 = Jsoup.connect(urlDetails).timeout(10 * 1000);
-                Document document2 = connect2.get();
-                Elements allDetailsForPhone = document2.getElementsByClass("js-table-preview");
-                ///phone.setProcessor(allDetailsForPhone.get(0).select("td").get(1).text());//processor
-                phone.setGraphics(allDetailsForPhone.get(0).select("td").get(3).text()); //graphics
-
-
-                //try {
-                double ram = findNumberInString(allDetailsForPhone.get(0).select("td").get(5).text());//ram
-                phone.setRam(ram);
-//                } catch (Exception ex) {
-//                    double[] tab = {2.0, 3.0, 3.5, 4.0, 6.0, 12.0};
-//                    Random r = new Random();
-//                    phone.setBuiltInMemory(tab[r.nextInt(6)]);
-//                }
-
-
-                //double builtInMemory =  findNumberInString(allDetailsForPhone.get(0).select("td").get(7).text());//builtInMemory
-//            String tempBuiltInMemory = allDetailsForPhone.get(0).select("td").get(7).text().trim();
-//            //System.out.println("Problem here: " + tempBuiltInMemory);
-//            if (tempBuiltInMemory.contains("+")) {
-//                int index = tempBuiltInMemory.indexOf("+");
-//                tempBuiltInMemory = tempBuiltInMemory.substring(0, index).trim();
-//            }
-//            tempBuiltInMemory = tempBuiltInMemory.substring(0, tempBuiltInMemory.length() - 2).replaceAll(",", ".");
-//            try {
-//                phone.setBuiltInMemory(Double.parseDouble(tempBuiltInMemory));
-//            } catch (Exception e) {
-//                double[] tab = {5.0, 6.0, 8.0, 16.0, 32.0};
-//                Random r = new Random();
-//                phone.setBuiltInMemory(tab[r.nextInt(5)]);
-//                System.out.println("Był ex" + e.getMessage() + " "+ tempBuiltInMemory);
-//                numberOfErrors+=1;
-//            }
-
-
-                phone.setTypeOfDisplay(allDetailsForPhone.get(0).select("td").get(9).text()); //type of display
-//            String tempSize = allDetailsForPhone.get(0).select("td").get(11).text();
-//            tempSize = tempSize.substring(0, tempSize.length() - 1);
-//            try {
-//                phone.setSizeOfDisplay(Double.parseDouble(tempSize));
-//            } catch (Exception ex) {
-//                double[] tab = {4.0, 4.5, 5.0, 5.0, 5.5, 6.0, 6.5, 7.0};
-//                Random r = new Random();
-//                phone.setSizeOfDisplay(tab[r.nextInt(8)]);
-//            }
-
-
-                phone.setResolutionOfDisplay(allDetailsForPhone.get(0).select("td").get(13).text());
-                phone.setCommunication(allDetailsForPhone.get(0).select("td").get(15).text());
-                phone.setNavigation(allDetailsForPhone.get(0).select("td").get(17).text());
-                phone.setConnectors(allDetailsForPhone.get(0).select("td").get(19).text());
-                //try {
-                phone.setCapacityOfBattery(findNumberInString(allDetailsForPhone.get(0).select("td").get(21).text()));
-//                } catch (Exception e) {
-//                    phone.setCapacityOfBattery(2500);
-//                    System.out.println("Był ex");
-//                    numberOfErrors += 1;
-//                }
-
-                //phone.setOperatingSystem(allDetailsForPhone.get(0).select("td").get(23).text());
-
-                String[] splitCamera = allDetailsForPhone.get(0).select("td").get(25).text().split("\\s");
-                //System.out.println(splitCamera[0]);
-                //try {
-                phone.setCameraMPX(Double.parseDouble(splitCamera[0]));
-//                } catch (Exception e) {
-//                    double[] tab = {5.0, 6.0, 8.0, 12.0};
-//                    Random r = new Random();
-//                    phone.setCameraMPX(tab[r.nextInt(4)]);
-//                    System.out.println("Był ex");
-//                    numberOfErrors += 1;
-//                }
-
-
-                //try {
-                phone.setFrontCameraMPX(Double.parseDouble(splitCamera[4]));
-//                } catch (Exception e) {
-//                    double[] tab = {2.0, 3.0, 4.5, 5.0, 8.0};
-//                    Random r = new Random();
-//                    phone.setFrontCameraMPX(tab[r.nextInt(5)]);
-//                    System.out.println("Był ex");
-//                    numberOfErrors += 1;
-//                }
-
-                phone.setFlashLamp(allDetailsForPhone.get(0).select("td").get(27).text());
-
-                String tempThickness = allDetailsForPhone.get(0).select("td").get(31).text().replaceAll(",", ".");
-                if (tempThickness.contains("m")) {
-                    int endIndex = tempThickness.indexOf("m");
-                    tempThickness = tempThickness.substring(0, endIndex);
+                if(!camera.isEmpty()){
+                    phone.setCameraMPX(camera.get(0));
                 }
-                //try {
-                phone.setThickness(findNumberInString(tempThickness)); //thickness in mm
-//                } catch (Exception e) {
-//                    //	8,95
-//                    double[] tab = {8.95, 8.5, 9.5, 9.0, 8.0, 9.5, 10.0, 7.5, 7.0};
-//                    Random r = new Random();
-//                    phone.setThickness(tab[r.nextInt(9)]);
-//                    System.out.println("Był ex");
-//                    numberOfErrors += 1;
-//                }
-
-                //try {
-                String tempWidth = allDetailsForPhone.get(0).select("td").get(33).text().replaceAll(",", ".");
-                phone.setWidth(findNumberInString(tempWidth)); //mm
-//                } catch (Exception e) {
-//                    double[] tab = {70.0, 71.0, 71.5, 72.5, 73.0, 76.5, 78.0, 78, 5, 80.0, 80.5, 81.5, 82.5, 84.5, 85.0};
-//                    Random r = new Random();
-//                    phone.setWidth(tab[r.nextInt(15)]);
-//                    System.out.println("Był ex");
-//                    numberOfErrors += 1;
-//                }
-
-
-                //try {
-                phone.setHeight(findNumberInString(allDetailsForPhone.get(0).select("td").get(35).text().replaceAll(",", "."))); //mm
-//                } catch (Exception e) {
-//                    double[] tab = {140.0, 144.0, 145.0, 145.5, 148.0, 148.5, 150.5, 160.5, 165.5, 170.6};
-//                    Random r = new Random();
-//                    phone.setHeight(tab[r.nextInt(10)]);
-//                    System.out.println("Był ex");
-//                    numberOfErrors += 1;
-//                }
-
-                //try {
-                phone.setWeight(findNumberInString(allDetailsForPhone.get(0).select("td").get(37).text().replaceAll(",", "."))); //g
-//                } catch (Exception e) {
-//                    double[] tab = {140.0, 144.0, 145.0, 145.5, 148.0, 148.5, 150.5, 160.5, 165.5, 170.6};
-//                    Random r = new Random();
-//                    phone.setWeight(tab[r.nextInt(10)]);
-//                    System.out.println("Był ex");
-//                    numberOfErrors += 1;
-//                }
-
-
-                if (39 <= allDetailsForPhone.get(0).select("td").size()) {
-                    phone.setColour(allDetailsForPhone.get(0).select("td").get(39).text());
+                if(camera.size()==2){
+                    phone.setFrontCameraMPX(camera.get(1));
                 }
-
-
-            } catch (Exception e) {
-                phone = null;
             }
-            if (phone != null) {
-                phones.add(phone);
+            if(key.contains("Bateria")){
+                String temp = value.replaceAll(",",".");
+                phone.setCapacityOfBattery(findNumberInString(temp));
+            }
+            if(key.contains("Cena")){
+                String temp = value.replaceAll(",",".");
+                phone.setPrice(Double.parseDouble(temp));
+            }
+            if(key.contains("Dodatkowe informacje")){
+
             }
         }
-        return phones;
     }
 
     private Double findNumberInString(String text) {
