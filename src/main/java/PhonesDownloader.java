ec96a18d6e6863e192c9d16f2545efb6191830e6
Marcin Giec <giecmarcin@gmail.com>
2016-12-20 20:22:24 +0100

    Remove problem with builtInMemory

--- src/main/java/PhonesDownloader.java
+++ src/main/java/PhonesDownloader.java
@@ -11,9 +11,10 @@ import java.util.regex.Pattern;
 public class PhonesDownloader {
     TreeMap<String, String> values = new TreeMap<>();
     List<TreeMap<String, String>> dataForPhones = new ArrayList<>();
+    int numberOflooseData=0;
 
     public TreeMap<String, String> download(String url) throws IOException {
-        Connection connect = Jsoup.connect(url).timeout(10 * 1000);
+        Connection connect = Jsoup.connect(url).timeout(10 * 2000);
         Document document = connect.get();
         Elements all = document.getElementsByClass("product-item");//.tagName("data-product-name");//data-products-list-name=
         List<String> urls = new ArrayList<>();
@@ -64,6 +65,9 @@ public class PhonesDownloader {
         List<Phone> phones = new ArrayList<>();
         Phone phone = null;
         for (TreeMap<String, String> dataForOnePhone : dataForPhones) {
+            int i=0;
+            i+=1;
+            System.out.println("Nr: + " + i);
             phone = new Phone();
             //System.out.println(dataForOnePhone.get("Cena").replaceAll(",","."));
             phone.setBrand(dataForOnePhone.get("Marka"));
@@ -72,6 +76,7 @@ public class PhonesDownloader {
             phone.setProcessor(dataForOnePhone.get("Procesor"));
             phone.setGraphics(dataForOnePhone.get("Układ graficzny"));
 
+            System.out.println(dataForOnePhone.get("Pamięć RAM"));
             double ram = findNumberInString(dataForOnePhone.get("Pamięć RAM").replaceAll(",","."));
             phone.setRam(ram);
             String tempBuiltMemory = dataForOnePhone.get("Pamięć wbudowana").replaceAll(",",".");
@@ -83,13 +88,23 @@ public class PhonesDownloader {
             }
             phone.setBuiltInMemory(findNumberInString(tempBuiltMemory));
             phone.setTypeOfDisplay(dataForOnePhone.get("Typ ekranu"));
-            phone.setSizeOfDisplay(findNumberInString(dataForOnePhone.get("Przekątna ekranu").substring(0, dataForOnePhone.get("Przekątna ekranu").length()-1)));
+            String tempDisplaySize = dataForOnePhone.get("Przekątna ekranu");
+//            if(tempDisplaySize.contains("\"")){
+//                int indexToRemove = tempDisplaySize.indexOf("\"");
+//                StringBuffer s = new StringBuffer(tempDisplaySize);
+//                s.deleteCharAt(indexToRemove);
+//                tempDisplaySize = s.toString();
+//            }
+            System.out.println("Test " + tempDisplaySize);
+            phone.setSizeOfDisplay(findNumberInString(tempDisplaySize));
             phone.setResolutionOfDisplay(dataForOnePhone.get("Rozdzielczość ekranu"));
             String tempCommunity[] = dataForOnePhone.get("Łączność").split("\\s+");
             phone.setCommunication(tempCommunity);
             phone.setNavigation(dataForOnePhone.get("System nawigacji satelitarnej"));
             phone.setConnectors(dataForOnePhone.get("Złącza"));
-            phone.setCapacityOfBattery(findNumberInString(dataForOnePhone.get("Bateria")));
+            System.out.println("Jest: " + dataForOnePhone.get("Bateria"));
+            //phone.setCapacityOfBattery(findNumberInString(dataForOnePhone.get("Bateria")));
+
             phone.setOperatingSystem(dataForOnePhone.get("Zainstalowany system operacyjny"));
 
             String tempCamera[] = dataForOnePhone.get("Aparat").replaceAll(",",".").split("-");
@@ -99,10 +114,12 @@ public class PhonesDownloader {
             }
             phone.setFlashLamp(dataForOnePhone.get("Lampa błyskowa"));
             phone.setResolutionRecordingVideo(dataForOnePhone.get("Rozdzielczość nagrywania wideo"));
-            phone.setThickness(findNumberInString(dataForOnePhone.get("Grubość").replaceAll(",",".")));
-            phone.setWidth(findNumberInString(dataForOnePhone.get("Szerokość").replaceAll(",",".")));
-            phone.setHeight(findNumberInString(dataForOnePhone.get("Wysokość").replaceAll(",",".")));
-            phone.setWeight(findNumberInString(dataForOnePhone.get("Waga").replaceAll(",",".")));
+
+            String thickness = dataForOnePhone.get("Grubość");
+            phone.setThickness(findNumberInString(thickness));
+            phone.setWidth(findNumberInString(dataForOnePhone.get("Szerokość")));
+            phone.setHeight(findNumberInString(dataForOnePhone.get("Wysokość")));
+            phone.setWeight(findNumberInString(dataForOnePhone.get("Waga")));
             phone.setColour(dataForOnePhone.get("Kolor"));
             phone.setExtraInfo(dataForOnePhone.get("Dodatkowe informacje"));
             phone.setIncludedAccessories(dataForOnePhone.get("Dołączone akcesoria"));
@@ -111,18 +128,53 @@ public class PhonesDownloader {
             //pobierz jeszcze zdjecia
             phones.add(phone);
         }
+        System.out.println("Stracone dane: " + numberOflooseData);
         return phones;
     }
 
     private Double findNumberInString(String text) {
-        StringBuffer sBuffer = new StringBuffer();
-        Pattern p = Pattern.compile("[0-9]+.[0-9]*|[0-9]*.[0-9]+|[0-9]+");
-        Matcher m = p.matcher(text);
-        while (m.find()) {
-            sBuffer.append(m.group());
+        try{
+            int numberOfDot=0;
+            for(char c : text.toCharArray()){
+                int number  = (int)c;
+                if(number==44){
+                    text = text.replaceAll(",",".");
+                }
+            }
+            for(char c : text.toCharArray()){
+                int number  = (int)c;
+                if(number==46) {
+                    numberOfDot += 1;
+                }
+            }
+            System.out.println("Number of dot kur.. " + numberOfDot + " text: " + text);
+            if(numberOfDot>1){
+                System.out.println("I m here");
+                int index = text.lastIndexOf(".");
+                System.out.println("Index " + index);
+                if(index!=-1){
 
+                    //System.out.println("Numer of dot: " + numberOfDot + " v: " + text);
+                    text = text.substring(0, index);
+                }
+
+            }
+            StringBuffer sBuffer = new StringBuffer();
+            //Pattern p = Pattern.compile("[0-9]+.[0-9]*|[0-9]*.[0-9]+|[0-9]+");
+            //Pattern p = Pattern.compile("[0-9]+.[0-9]*");
+            //Pattern  p = Pattern.compile("[0-9]+\\.[0-9]*");
+            Pattern p = Pattern.compile("\\d+\\.?\\d*|\\.\\d+");
+            Matcher m = p.matcher(text);
+            while (m.find()) {
+                sBuffer.append(m.group());
+
+            }
+            System.out.println(sBuffer);
+            return Double.parseDouble(sBuffer.toString());
+        }catch (Exception e){
+            numberOflooseData+=1;
+            text="0";
+            return Double.parseDouble(text);
         }
-        System.out.println(sBuffer);
-        return Double.parseDouble(sBuffer.toString());
     }
 }
